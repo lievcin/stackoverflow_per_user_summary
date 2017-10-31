1caeb3528916edc75dc30f0c2ed689b364dbf04b
Liev Efraim Garcia <liev.garcia@gmail.com>
2017-10-31 11:32:20 +0000

    final commit:

--- src/TweetsParser.java
+++ src/TweetsParser.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.io.Text;
+import java.text.Normalizer;
 
 public class TweetsParser {
 
@@ -13,17 +14,25 @@ public class TweetsParser {
 
 		tweet_parts = tweet.toString().split("\\;");
 
-		if (tweet_parts.length == 4 && tweet_parts[0].charAt(0) != '#') {
+		if (tweet_parts.length == 4) {
 			//the condition above is due to a series of tweets that have no id somehow or are corrupted
 			//but they have 4 elements as ; is being used inside the text of the tweet itself.
-			epoch = Long.parseLong(tweet_parts[0]);
-			tweetId = tweet_parts[1];
-			tweetBody = tweet_parts[2];
-			device = tweet_parts[3];
-			valid_structure = true;
+
+			try {
+        epoch = Long.parseLong(tweet_parts[0]);
+				tweetId = tweet_parts[1];
+				//there would be more work to do here to remove some accents as well as non-standard characters
+				//however due to the scope of the exercise this is not really addressed in this task.
+				tweetBody = tweet_parts[2];
+				device = tweet_parts[3];
+				valid_structure = true;
+      } catch(Exception e) {
+      	valid_structure  = false; //unexpected characters in a field (like text in an epoch)
+  	  }
+
 		}
 		else {
-			valid_structure  = false;
+			valid_structure  = false; // when split not getting valid number of fields
 		}
 
 	}
@@ -46,7 +55,7 @@ public class TweetsParser {
 
 	public boolean isValidTweet() {
 
-		if (valid_structure == false || getTweetLength() > 140) {
+		if (valid_structure == false || getTweetLength() > 140 ) {
 			return false;
 		}
 		else {
